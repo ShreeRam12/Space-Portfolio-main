5b98f927610b3741598a1c3038769fbe0afb1391
ShreeRam12 <shreeramshree109@gmail.com>
2025-08-31 21:06:59 +0530

    changes updated

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -15,22 +15,23 @@ const HeroContent = () => {
     <motion.div
       initial="hidden"
       animate="visible"
-      className="flex flex-row items-center justify-center px-20 mt-40 w-full z-[20]"
+      className="flex flex-col-reverse lg:flex-row items-center justify-center px-6 sm:px-12 md:px-16 lg:px-20 mt-20 md:mt-32 lg:mt-40 w-full z-[20] gap-10"
     >
-      <div className="h-full w-full flex flex-col gap-5 justify-center m-auto text-start">
+      {/* Left Section */}
+      <div className="h-full w-full flex flex-col gap-5 justify-center m-auto text-center lg:text-start">
         <motion.div
           variants={slideInFromTop}
-          className="Welcome-box py-[8px] px-[7px] border border-[#7042f88b] opacity-[0.9]"
+          className="Welcome-box py-[8px] px-[12px] border border-[#7042f88b] opacity-[0.9] mx-auto lg:mx-0 flex items-center gap-2 rounded-md"
         >
-          <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
-          <h1 className="Welcome-text text-[13px]">
+          <SparklesIcon className="text-[#b49bff] h-5 w-5" />
+          <h1 className="Welcome-text text-[13px] sm:text-[15px]">
             Fullstack Developer Portfolio
           </h1>
         </motion.div>
 
         <motion.div
           variants={slideInFromLeft(0.5)}
-          className="flex flex-col gap-6 mt-6 text-6xl font-bold text-white max-w-[600px] w-auto h-auto"
+          className="flex flex-col gap-6 mt-6 text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold text-white max-w-[600px] w-auto h-auto"
         >
           <span>
             Shree
@@ -44,19 +45,24 @@ const HeroContent = () => {
 
         <motion.p
           variants={slideInFromLeft(0.8)}
-          className="text-lg text-gray-400 my-5 max-w-[600px]"
+          className="text-sm sm:text-base md:text-lg text-gray-400 my-5 max-w-[600px] mx-auto lg:mx-0"
         >
           I&apos;m a Full Stack Software Engineer with experience in Website,
           Mobile, and Software development. Check out my projects and skills.
         </motion.p>
+
+        {/* Download Resume Button */}
         <motion.a
           variants={slideInFromLeft(1)}
+          href="/K_ShreeRam_Resume(1).docx" // place your resume file inside public/ folder
+          download="ShreeRam_Resume.docx"
           className="py-2 button-primary text-center text-white cursor-pointer rounded-lg max-w-[200px]"
         >
-          Learn More!
+          Download Resume
         </motion.a>
       </div>
 
+      {/* Right Section */}
       <motion.div
         variants={slideInFromRight(0.8)}
         className="w-full h-full flex justify-center items-center"
@@ -64,8 +70,9 @@ const HeroContent = () => {
         <Image
           src="/mainIconsdark.svg"
           alt="work icons"
-          height={650}
-          width={650}
+          height={500}
+          width={500}
+          className="w-[250px] sm:w-[350px] md:w-[450px] lg:w-[550px] xl:w-[650px] h-auto"
         />
       </motion.div>
     </motion.div>
