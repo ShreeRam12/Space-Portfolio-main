ed297425a654f3852db4f0acc8b356267bd9aa63
ShreeRam12 <shreeramshree109@gmail.com>
2025-08-31 18:08:26 +0530

    minor changes updated

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-        "./pages/**/*.{js,ts,jsx,tsx}",
-        "./components/**/*.{js,ts,jsx,tsx}",
-        "./app/**/*.{js,ts,jsx,tsx}", // if using Next.js app router
+        
     ],
     theme: {
         extend: {},
